7fabad7fb73e9dc7f563c048143f311a813c6208
Andrew Williams <sisaw7@raven13.bullx>
2015-10-02 09:51:23 +0100

    Makefiled the Summation C++ code. Segfaults when running, so this needs looking into once all the code builds correctly.

--- Summation/Summation-CXX/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash --login
-rm -f SummationCXX
-rm -f SummationCXX.log.*
-rm -f SummationCXX.out
-rm -f SummationCXX.err
-rm -f *~
--- Summation/Summation-CXX/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash --login
-module purge
-module load compiler/intel
-module load mpi/intel
-
-mpicxx Summation.cxx -o SummationCXX
