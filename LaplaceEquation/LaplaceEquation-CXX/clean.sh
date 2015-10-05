495268b97b7163f65a3182ebcf8a943117b8789f
Andrew Williams <sisaw7@raven13.bullx>
2015-10-05 09:57:23 +0100

    Added Makefile for C++ LaplaceEquation.

--- LaplaceEquation/LaplaceEquation-CXX/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash --login
-rm -f LaplaceEquationCXX
-rm -f LaplaceEquationCXX.log.*
-rm -f LaplaceEquationCXX.out
-rm -f LaplaceEquationCXX.err
-rm -f *~
--- LaplaceEquation/LaplaceEquation-CXX/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash --login
-module purge
-module load compiler/intel
-module load mpi/intel
-
-mpicxx LaplaceEquation.cxx -o LaplaceEquationCXX
