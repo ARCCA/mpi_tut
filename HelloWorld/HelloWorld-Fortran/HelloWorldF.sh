9ad4e7a49d3f17d3e117a6122d8ddb347a02760c
Andrew Williams <sisaw7@raven13.bullx>
2015-10-06 12:48:48 +0100

    Added HelloWorld C++ and Fortran PBS scripts

---
+++ HelloWorld/HelloWorld-Fortran/HelloWorldF.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=16:mpiprocs=16:mem=10MB
+#PBS -l walltime=00:00:30
+#PBS -N HelloWorldF
+#PBS -o HelloWorldF.out
+#PBS -e HelloWorldF.err
+#PBS -P PR39
+#PBS -q workq
+
+# Load required modules.
+module purge
+module load mpi/intel
+
+# Create an output directory on the fast scratch filesystem, and
+# run from this directory.
+WDPATH=/scratch/$USER/raven_training/$PBS_JOBID
+mkdir -p $WDPATH
+cd $WDPATH
+
+# Run a number of copies of the code equal to the number of
+# MPI processes requested.
+mpirun -np 16 $PBS_O_WORKDIR/HelloWorld >& HelloWorld.log.$PBS_JOBID
