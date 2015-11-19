0c7d275842a4b9ce33136131295f1dd062e410c5
Andrew Williams <williamsa89@cardiff.ac.uk>
2015-11-19 15:40:49 +0000

    Added section to the vibrating string visualisation script to use ImageMagick to convert the separate png files into an animated gif

--- VibratingString/VibratingString-VizTool/run.sh
+++ VibratingString/VibratingString-VizTool/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash --login
+# Load the modules we require
 module load java
+module load ImageMagick
+
+# Plot the output using a Java application
 java -classpath src uk/co/hpcwales/vibratingstring/VizTool output-0.txt
+
+# Convert the separate image files into a single animation using ImageMagick
+convert -delay 10 -loop 0 image* stringanim.gif
