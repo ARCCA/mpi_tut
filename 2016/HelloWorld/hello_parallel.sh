92127397b4e0fd867f94ea5c4abedea4ab8b26d3
Andrew Williams <williamsa89@cardiff.ac.uk>
2016-11-23 12:08:16 +0000

    ensure all job scripts use a consistent format

--- 2016/HelloWorld/hello_parallel.sh
+++ 2016/HelloWorld/hello_parallel.sh
@@ -2,8 +2,6 @@
 #PBS -l select=1:ncpus=16:mpiprocs=16:mem=10MB
 #PBS -l walltime=00:00:30
 #PBS -N HelloPython
-#PBS -o HelloPython.out
-#PBS -e HelloPython.err
 #PBS -P PR66
 #PBS -q training
 
@@ -17,6 +15,9 @@ WDPATH=/scratch/$USER/raven_training/$PBS_JOBID
 mkdir -p $WDPATH
 cd $WDPATH
 
+# Copy the python code to the run directory
+cp $PBS_O_WORKDIR/hello_parallel.py .
+
 # Run a number of copies of the code equal to the number of
 # MPI processes requested.
-mpirun -np 16 $PBS_O_WORKDIR/hello_parallel.py >& hello_parallel.log.$PBS_JOBID
+mpirun -np 16 ./hello_parallel.py
