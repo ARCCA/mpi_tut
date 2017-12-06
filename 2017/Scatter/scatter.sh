86a29497655775f1a9a91c00aa9ab9f3fcc31e0f
Andrew Williams <williamsa89@cardiff.ac.uk>
2017-12-06 13:53:40 +0000

    scatter updated

--- 2017/Scatter/scatter.sh
+++ 2017/Scatter/scatter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=16:mpiprocs=16:mem=10MB
+#PBS -l select=1:ncpus=12:mpiprocs=12:mem=1MB
 #PBS -l walltime=00:00:30
 #PBS -N Scatter
 #PBS -P PR66
@@ -20,4 +20,4 @@ cp $PBS_O_WORKDIR/scatter.py .
 
 # Run a number of copies of the code equal to the number of
 # MPI processes requested.
-mpirun -np 16 ./scatter.py
+mpirun -np 12 ./scatter.py
