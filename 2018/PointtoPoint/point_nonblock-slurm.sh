339fb13cb79779ae644cb334a5cbcd2959363abe
Thomas Green <greent10@cardiff.ac.uk>
2018-11-28 12:16:37 +0000

    Create SLURM scripts.

---
+++ 2018/PointtoPoint/point_nonblock-slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+#SBATCH -n 12
+#SBATCH -t 0-00:30
+#SBATCH -N PointNonBlocking
+#SBATCH --account scw1148
+#SBATCH -p compute
+
+# Load required modules.
+module purge
+module load raven
+module load python/2.7.9-mpi
+
+# Create an output directory on the fast scratch filesystem, and
+# run from this directory.
+WDPATH=/scratch/$USER/mpi_training/$SLURM_JOBID
+mkdir -p $WDPATH
+cd $WDPATH
+
+# Copy the python code to the run directory
+cp $SLURM_SUBMIT_DIR/point_nonblock.py .
+
+# Run a number of copies of the code equal to the number of
+# MPI processes requested.
+mpirun -np 12 ./point_nonblock.py
---
+++ 2018/SineIntegral/sine-slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+#SBATCH -n 4
+#SBATCH -t 0-00:30
+#SBATCH -N SineIntegral
+#SBATCH --account scw1148
+#SBATCH -p compute
+
+# Load required modules.
+module purge
+module load raven
+module load python/2.7.9-mpi
+
+# Create an output directory on the fast scratch filesystem, and
+# run from this directory.
+WDPATH=/scratch/$USER/raven_training/$SLURM_JOBID
+mkdir -p $WDPATH
+cd $WDPATH
+
+# Copy the python code to the run directory
+cp $SLURM_SUBMIT_DIR/sine.py .
+
+# Run a number of copies of the code equal to the number of
+# MPI processes requested.
+mpirun -np 4 ./sine.py
